1df296d06bd92959cde7aa35bcb172c13bdcace1
caiwei <caiwei@baibu.la>
2020-07-16 18:25:16 +0800

    feat:创建项目并引入ts

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import Navigation from './app/Navigation';
+import { name as appName } from './app.json';
+AppRegistry.registerComponent(appName, () => Navigation);
